d7ae1186e90c0040f22d1e6c56f6e4e71940dbe8
xiongmeiqiang <xiongmeiqiang_w@coolchuan.net>
2018-07-19 08:34:07 +0800

    test

--- src/WordSearch/index.ts
+++ src/WordSearch/index.ts
@@ -1,175 +1,180 @@
-type IMap = string[][];
+function exist(board, word){
 
-class Board {
-  private map: IMap;
-  private maxX: number;
-  private maxY: number;
-  private x: number = 0;
-  private y: number = 0;
-  private initPositons: { x: number; y: number }[] = [];
-  private markedPostions: boolean[][] = [];
-  constructor(map: IMap) {
-    this.map = map;
-    this.maxY = map.length - 1;
-    this.maxX = map[0].length - 1;
+  type IMap = string[][];
 
-    this.reInit(0, 0);
-  }
-
-  reInit(x: number, y: number) {
-    let col = [];
-    this.markedPostions = [];
-
-    for (let index = 0; index < this.maxY + 1; index++) {
-      this.markedPostions[index] = [];
+  class Board {
+    private map: IMap;
+    private maxX: number;
+    private maxY: number;
+    private x: number = 0;
+    private y: number = 0;
+    private initPositons: { x: number; y: number }[] = [];
+    private markedPostions: boolean[][] = [];
+    constructor(map: IMap) {
+      this.map = map;
+      this.maxY = map.length - 1;
+      this.maxX = map[0].length - 1;
+  
+      this.reInit(0, 0);
     }
-
-    this.moveToNewPosition(x, y);
-  }
-  moveToNewPosition(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-    this.mark(x, y);
-  }
-  getChartByPosition(x: number, y: number): string {
-    if (y < 0 || y > this.maxY) {
-      return undefined;
+  
+    reInit(x: number, y: number) {
+      let col = [];
+      this.markedPostions = [];
+  
+      for (let index = 0; index < this.maxY + 1; index++) {
+        this.markedPostions[index] = [];
+      }
+  
+      this.moveToNewPosition(x, y);
     }
-    if (x < 0 || x > this.maxX) {
-      return undefined;
+    moveToNewPosition(x: number, y: number) {
+      this.x = x;
+      this.y = y;
+      this.mark(x, y);
     }
-    return this.map[y][x];
-  }
-
-  getPositionsByChart(chart: string): { x: number; y: number }[] {
-    for (let y = 0; y < this.maxY; y++) {
-      const element = this.map[y];
-      for (let x = 0; x < element.length; x++) {
-        const c = element[x];
-        if (c === chart) {
-          this.initPositons.push({ x, y });
+    getChartByPosition(x: number, y: number): string {
+      if (y < 0 || y > this.maxY) {
+        return undefined;
+      }
+      if (x < 0 || x > this.maxX) {
+        return undefined;
+      }
+      return this.map[y][x];
+    }
+  
+    getPositionsByChart(chart: string): { x: number; y: number }[] {
+      for (let y = 0; y <= this.maxY; y++) {
+        const element = this.map[y];
+        for (let x = 0; x <= this.maxX; x++) {
+          const c = element[x];
+          
+          if (c === chart) {
+            this.initPositons.push({ x, y });
+          }
         }
       }
+      return this.initPositons;
     }
-    return this.initPositons;
+  
+    mark(x: number, y: number) {
+      if (y < 0 || y > this.maxY) {
+        return undefined;
+      }
+      if (x < 0 || x > this.maxX) {
+        return undefined;
+      }
+      //   console.log(this);
+  
+      this.markedPostions[y][x] = true;
+    }
+  
+    isPositionMarked(x: number, y: number): boolean {
+      if (y < 0 || y > this.maxY) {
+        return false;
+      }
+      if (x < 0 || x > this.maxX) {
+        return false;
+      }
+      return this.markedPostions[y][x];
+    }
+    
+    getLeft(): { chart: string | undefined; x: number; y: number } {
+      let x = this.x - 1,
+        y = this.y;
+      let chart = this.getChartByPosition(x, y);
+      return {
+        chart,
+        x,
+        y
+      };
+    }
+  
+    getRight(): { chart: string | undefined; x: number; y: number } {
+      let x = this.x + 1,
+        y = this.y;
+      let chart = this.getChartByPosition(x, y);
+      return {
+        chart,
+        x,
+        y
+      };
+    }
+    getTop(): { chart: string | undefined; x: number; y: number } {
+      let x = this.x,
+        y = this.y - 1;
+      let chart = this.getChartByPosition(x, y);
+      return {
+        chart,
+        x,
+        y
+      };
+    }
+    getBottom(): { chart: string | undefined; x: number; y: number } {
+      let x = this.x,
+        y = this.y + 1;
+      let chart = this.getChartByPosition(x, y);
+      return {
+        chart,
+        x,
+        y
+      };
+    }
   }
-
-  mark(x: number, y: number) {
-    if (y < 0 || y > this.maxY) {
-      return undefined;
-    }
-    if (x < 0 || x > this.maxX) {
-      return undefined;
-    }
-    //   console.log(this);
-
-    this.markedPostions[y][x] = true;
-  }
-
-  isPositionMarked(x: number, y: number): boolean {
-    if (y < 0 || y > this.maxY) {
+  
+  function main(map: string[][], str: string) {
+    // const map = [["a", "a", "c"], ["d", "c", "e"], ["s", "d", "p"]];
+    let board = new Board(map);
+    let start = str[0];
+    str = str.substr(1, str.length - 1);
+  
+    let postions = board.getPositionsByChart(start);
+    
+    function getResult(str: string, board: Board): boolean {
+      if (!str.length) {
+        return true;
+      }
+      let c = str[0];
+      let _str = str.substr(1, str.length - 1);
+  
+      
+      let top = board.getTop();
+      let right = board.getRight();
+      let bottom = board.getBottom();
+      let left = board.getLeft();
+  
+      if (top.chart === c && !board.isPositionMarked(top.x, top.y)) {
+        board.moveToNewPosition(top.x, top.y);
+        return getResult(_str, board);
+      }
+      if (right.chart === c && !board.isPositionMarked(right.x, right.y)) {
+        board.moveToNewPosition(right.x, right.y);
+        return getResult(_str, board);
+      }
+      if (bottom.chart === c && !board.isPositionMarked(bottom.x, bottom.y)) {
+        board.moveToNewPosition(bottom.x, bottom.y);
+        return getResult(_str, board);
+      }
+      if (left.chart === c && !board.isPositionMarked(left.x, left.y)) {
+        board.moveToNewPosition(left.x, left.y);
+        return getResult(_str, board);
+      }
       return false;
     }
-    if (x < 0 || x > this.maxX) {
-      return false;
-    }
-    return this.markedPostions[y][x];
+    let result = postions.map(postion => {
+      board.reInit(postion.x, postion.y);
+      return getResult(str, board);
+    });
+  
+  
+  
+    return !!result.filter(f => f).length;
   }
-
-  getLeft(): { chart: string | undefined; x: number; y: number } {
-    let x = this.x - 1,
-      y = this.y;
-    let chart = this.getChartByPosition(x, y);
-    return {
-      chart,
-      x,
-      y
-    };
-  }
-  getRight(): { chart: string | undefined; x: number; y: number } {
-    let x = this.x + 1,
-      y = this.y;
-    let chart = this.getChartByPosition(x, y);
-    return {
-      chart,
-      x,
-      y
-    };
-  }
-  getTop(): { chart: string | undefined; x: number; y: number } {
-    let x = this.x,
-      y = this.y - 1;
-    let chart = this.getChartByPosition(x, y);
-    return {
-      chart,
-      x,
-      y
-    };
-  }
-  getBottom(): { chart: string | undefined; x: number; y: number } {
-    let x = this.x,
-      y = this.y + 1;
-    let chart = this.getChartByPosition(x, y);
-    return {
-      chart,
-      x,
-      y
-    };
-  }
-  getNextPostionByChartAndBoard(chart: string) {
-    let isMarked: boolean;
-    let top = this.getTop();
-  }
+  
+  
+  return main(board, word);
 }
 
-function main(str: string) {
-  const map = [["a", "a", "c"], ["d", "c", "e"], ["s", "d", "p"]];
-  let board = new Board(map);
-  let start = str[0];
-  str = str.substr(1, str.length - 1);
-
-  let postions = board.getPositionsByChart(start);
-
-  function getResult(str: string, board: Board): boolean {
-    let c = str[0];
-    let _str = str.substr(1, str.length - 1);
-
-    if (!str.length) {
-      return true;
-    }
-    let top = board.getTop();
-    let right = board.getRight();
-    let bottom = board.getBottom();
-    let left = board.getLeft();
-
-    if (top.chart === c && !board.isPositionMarked(top.x, top.y)) {
-      board.moveToNewPosition(top.x, top.y);
-      return getResult(_str, board);
-    }
-    if (right.chart === c && !board.isPositionMarked(right.x, right.y)) {
-      board.moveToNewPosition(right.x, right.y);
-      return getResult(_str, board);
-    }
-    if (bottom.chart === c && !board.isPositionMarked(bottom.x, bottom.y)) {
-      board.moveToNewPosition(bottom.x, bottom.y);
-      return getResult(_str, board);
-    }
-    if (left.chart === c && !board.isPositionMarked(left.x, left.y)) {
-      board.moveToNewPosition(left.x, left.y);
-      return getResult(_str, board);
-    }
-    return false;
-  }
-  let result = postions.map(postion => {
-    board.reInit(postion.x, postion.y);
-    return getResult(str, board);
-  });
-
-  //   console.log(result, 'result');
-
-  return !!result.filter(f => f).length;
-}
-
-let str = "aac";
-let result = main(str);
-console.log(result, "result");
+let a = exist([["C","A","A"],["A","A","A"],["B","C","D"]]
+, "AAB")
+console.log(a, 'a');
